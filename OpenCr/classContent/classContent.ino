c4fa3764299f299f635fc66b75b8196c87aab4ee
Pringels4u <mathiasdb20@gmail.com>
2025-10-23 13:47:27 +0200

    yes

---
+++ OpenCr/classContent/classContent.ino
@@ -0,0 +1,73 @@
+
+#include "songs.h"
+#include "sensors.h"
+#include "dynamix.h"
+
+int criticalNote = NOTE_C4;
+float criticalVoltage = 11.5 ;
+
+unsigned long lastBatteryCheck = 0;
+bool dynamixelStarted =  false;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Launching IMU");
+  IMU.begin();
+  Serial.println("Playing start melody");
+  playSequence(HappyBirthday,noteDurations_HappyBirthday);
+  pinMode( led_pin, OUTPUT );
+  
+  
+  dynamixelStarted = checkDynamixel();
+
+  if (dynamixelStarted == false) {
+    Serial.println("Dynamixels not initiated.");
+  }
+
+}
+
+bool checkCriticalBattery(){
+  float voltage = getBatteryVoltage();
+  Serial.println("Executed: checkCriticalBattery ");
+
+  if (voltage < criticalVoltage) {
+    tone(BDPIN_BUZZER, criticalNote, 100);
+    return true;
+  }
+
+  return false;
+}
+
+
+void printIMU(){
+    float roll = getRoll();
+    float yaw = getYaw();
+    float pitch = getPitch();
+    Serial.println("Roll:" + String(roll) + ",Pitch:" + String(pitch) + ",Yaw:" + String(yaw));
+}
+
+
+void loop() {
+
+
+    bool criticalVoltage = false;
+
+    float voltage = getBatteryVoltage();
+    
+    // Controleer of er 60 seconden voorbij zijn sinds de laatste batterijcheck
+    if (millis() - lastBatteryCheck >= 60000) {
+        lastBatteryCheck = millis();           // Reset timer
+        criticalVoltage = checkCriticalBattery();  // Controleer opnieuw
+    }
+
+    if (dynamixelStarted == false) {
+      Serial.println("Problems with dynamixels");
+      delay(1000);
+    }
+
+    printIMU();
+    Serial.println("Voltage: " + String(voltage) + " V");
+
+    delay(1000);  
+}
